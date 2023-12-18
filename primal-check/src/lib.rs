48526bbba548946ef85d6736b9e69544dffb2100
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2023-12-18 01:03:15 +0100

    Rename limb feature to no-std

--- primal-check/src/lib.rs
+++ primal-check/src/lib.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(feature = "no-std", no_std)]
 //! Check some primality-related properties of numbers.
 //!
 //! This crate is designed to be used via `primal`.
